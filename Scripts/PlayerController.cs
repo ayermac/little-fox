f4102fbed6dd3ab4e3fbbb345d7849c5af6cf0c3
jason <codemac@163.com>
2020-06-16 22:57:19 +0800

    initial

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalmovel;
+        horizontalmovel = Input.GetAxis("Horizontal");
+
+        if (horizontalmovel != 0)
+        {
+            rb.velocity = new Vector2(horizontalmovel * speed, rb.velocity.y);
+        }
+    }
+}
