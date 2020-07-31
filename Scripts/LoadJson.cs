f202e97b26f93866ad6645cfbf63009431e45080
jason <codemac@163.com>
2020-07-31 23:15:16 +0800

    json读取写入

---
+++ Scripts/LoadJson.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class LoadJson : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public static GameStatus LoadJsonFromFile()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        string file = Application.dataPath + "/Resources/demo.json";
+
+        if (!File.Exists(file))
+        {
+            return null;
+        }
+
+        StreamReader sr = new StreamReader(file);
+
+        if (sr == null)
+        {
+            return null;
+        }
+        string json = sr.ReadToEnd();
+        Debug.Log(json);
+
+        if (json.Length > 0)
+        {
+            return JsonUtility.FromJson<GameStatus>(json);
+        }
+
+        return null;
+    }
+}
